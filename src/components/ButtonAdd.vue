d92646a029494a51f41c26329d1cd71f59c3b26f
Nirajan Karki <nirajankarki1985@gmail.com>
2023-01-05 22:54:20 +0100

    first commit

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonAdd",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/HeaderTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonAdd text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import ButtonAdd from "./ButtonAdd.vue";
+
+export default {
+  name: "HeaderTask",
+  props: {
+    title: String,
+  },
+  components: {
+    ButtonAdd,
+  },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
